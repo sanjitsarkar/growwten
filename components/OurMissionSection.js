3ad918f5965d8f68978d3aa22c037fbf4b1c4ad1
Sanjit Sarkar <ssanjit856@gmail.com>
2021-09-09 11:17:57 +0530

    added config

--- components/OurMissionSection.js
+++ components/OurMissionSection.js
@@ -4,7 +4,7 @@ const OurMissionSection = () => {
            <h1 className="text-center text-2xl text-white font-bold">Our Mission</h1>
            <div className="card card-mission bg-secondary rounded-md shadow-lg p-6 md:p-7 cursor-pointer transform transition  scale-95 duration-300 hover:scale-100 hover:shadow-2xl">
                <p className="text-white font-sm text-md md:text-lg leading-relaxed ">
-               We have created a way for everyone to change their lives financially by making 10 people's group without having to invest large amount of money (zero Investment). To change people's lives, should be possible without affecting their daily budget.
+               We have created a way for everyone to change their lives financially by making 10 people&apos;s group without having to invest large amount of money (zero Investment). To change people&apos; lives, should be possible without affecting their daily budget.
                </p>
            </div>
         </div>
